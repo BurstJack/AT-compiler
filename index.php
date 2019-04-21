a39940c6763c63991ce0b0af767a4a4193b1c2d1
bruker <bruker@ThinkPad-1>
2019-04-21 05:55:53 +0200

    21-04-19 (4)

--- index.php
+++ index.php
@@ -10,16 +10,16 @@
   <style>
      body { font-family: 'Teko'; font-family: Arial, Helvetica, sans-serif; }
      .tab-pane { padding: 20px;background-color:#dff0d8; }
-h2 { 
-  display: block;
-  font-size: 1.85em;
-  margin-top: 0.83em;
-  margin-bottom: 0.83em;
-  margin-left: 0;
-  margin-right: 0;
-  font-weight: bold;
-  padding: 0 10px;
-}
+     h2, .heading { 
+       display: block;
+       font-size: 1.65em;
+       margin-top: 0.63em;
+       margin-bottom: 0.63em;
+       margin-left: 0;
+       margin-right: 0;
+       font-weight: bold;
+       //padding: 0 10px;
+     }
      button {              
         border-color: #777777;
         color:#fff;
@@ -32,7 +32,7 @@ h2 {
         text-decoration: none;
         display: inline-block;
         font-size: 14px;
-        margin: 4px 2px;
+        margin: 8px 0 2px 1px;
      } 
      button:hover { background-color: green; }
 
@@ -54,38 +54,38 @@ h2 {
 <body>
 
 <div class="container">
-  <h2>API Tester..</h2>
-  <p style="padding: 20px;background-color: #dff0d8;">some text</p>
+  <!-- <h2>API Tester..</h2> --><br>
+  <p class="heading" style="padding: 20px;background-color: #dff0d8;">AT compiler</p>
 
   <ul class="nav nav-tabs">
-    <li class="active"><a data-toggle="tab" href="#test1">Test 1</a></li>
-    <li><a data-toggle="tab" href="#test2">Test 2</a></li>
-    <li><a data-toggle="tab" href="#test3">Test 3</a></li>
-    <li><a data-toggle="tab" href="#test4">Test 4</a></li>
+    <li class="active"><a data-toggle="tab" href="#test1">Scr-Asm</a></li>
+    <li><a data-toggle="tab" href="#test2">Mcode</a></li>
+    <li><a data-toggle="tab" href="#test3">Bcode</a></li>
+    <li><a data-toggle="tab" href="#test4">Steps</a></li>
   </ul>
 
   <div class="tab-content">
-    <div id="test1" class="tab-pane fade in active">
-      <h4>Test 1</h4>
-      <textarea id="ta1" style="width:100%; height:300px;padding:5px;">input...test1</textarea>
+    <div id="test1" class="tab-pane fade in active" style="padding-bottom:10px;">
+      <h4>Scr-Asm</h4>
+      <textarea id="ta1" style="width:100%; height:315px;padding:5px;">input...test1</textarea>
       <button onclick="test1();">Submit 1</button>
     </div>
 
-    <div id="test2" class="tab-pane fade" style="padding: 20px;">
-      <h4>Test 2</h4>
-      <textarea id="ta2"style="width:100%; height:300px;padding:5px;;">input..test2</textarea>
+    <div id="test2" class="tab-pane fade" style="padding-bottom:10px;">
+      <h4>Mcode</h4>
+      <textarea id="ta2"style="width:100%; height:315px;padding:5px;">input..test2</textarea>
       <button onclick="test2();">Submit 2</button>
     </div>
   
-    <div id="test3" class="tab-pane fade" style="padding: 20px;">
-      <h4>Test 3</h4>
-      <textarea id="ta3"style="width:100%; height:300px;padding:5px;;">input..test3</textarea>
+    <div id="test3" class="tab-pane fade" style="padding-bottom:10px;">
+      <h4>Bcode</h4>
+      <textarea id="ta3"style="width:100%; height:315px;padding:5px;">input..test3</textarea>
       <button onclick="test3();">Submit 3</button>
   </div>
   
-    <div id="test4" class="tab-pane fade" style="padding: 20px;">
-      <h4>Test 4</h4>
-      <textarea id="ta4" style="width:100%; height:300px;padding:5px;;">input..test4</textarea>
+    <div id="test4" class="tab-pane fade" style="padding-bottom:10px;">
+      <h4>Steps</h4>
+      <textarea id="ta4" style="width:100%; height:315px;padding:5px;">input..test4</textarea>
       <button onclick="test4();">Submit 4</button>
     </div>
   </div>
@@ -100,7 +100,7 @@ h2 {
       var i = document.getElementById("ta1").value;
       r = "test1";
 
-      s.src = "http://atburst.atwebpages.com/at/rosetta/server.php?req=test1&inp="+i;
+      s.src = "http://atburst.atwebpages.com/at/rosetta/server.php?req=scr-asm&inp="+i;
       document.body.appendChild(s); 
    }
 
@@ -109,7 +109,7 @@ h2 {
       var i = document.getElementById("ta2").value;
       r = "test2";
 
-      s.src = "http://atburst.atwebpages.com/at/rosetta/server.php?req=test2&inp="+i;
+      s.src = "http://atburst.atwebpages.com/at/rosetta/server.php?req=mcode&inp="+i;
       document.body.appendChild(s); 
    }
 
@@ -118,7 +118,7 @@ h2 {
       var i = document.getElementById("ta3").value;
       r = "test3";
 
-      s.src = "http://atburst.atwebpages.com/at/rosetta/server.php?req=test3&inp="+i;
+      s.src = "http://atburst.atwebpages.com/at/rosetta/server.php?req=bcode&inp="+i;
       document.body.appendChild(s); 
    }
 
@@ -127,7 +127,7 @@ h2 {
       var i = document.getElementById("ta4").value;
       r = "test4";
 
-      s.src = "http://atburst.atwebpages.com/at/rosetta/server.php?req=test4&inp="+i;
+      s.src = "http://atburst.atwebpages.com/at/rosetta/server.php?req=steps&inp="+i;
       document.body.appendChild(s); 
    }
 
