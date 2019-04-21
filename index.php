4308dc9e9f8f3aa7754b1c4754bd10c4d4f69058
bruker <bruker@ThinkPad-1>
2019-04-21 05:08:34 +0200

    21-04-19 (3)

--- index.php
+++ index.php
@@ -10,12 +10,51 @@
   <style>
      body { font-family: 'Teko'; font-family: Arial, Helvetica, sans-serif; }
      .tab-pane { padding: 20px;background-color:#dff0d8; }
+h2 { 
+  display: block;
+  font-size: 1.85em;
+  margin-top: 0.83em;
+  margin-bottom: 0.83em;
+  margin-left: 0;
+  margin-right: 0;
+  font-weight: bold;
+  padding: 0 10px;
+}
+     button {              
+        border-color: #777777;
+        color:#fff;
+        border-radius: 4px;
+        background-color: #4CAF50;
+        border: none;
+        color: white;
+        padding: 8px 12px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 14px;
+        margin: 4px 2px;
+     } 
+     button:hover { background-color: green; }
+
+     /* tab color */
+     .nav-tabs>li>a {
+      margin: 10px 1px 0 1px;
+      background-color: #dff0d8; //#e7e7e7; 
+      color: black;
+     } 
+  
+     /* active tab color */
+     .nav-tabs>li.active>a, .nav-tabs>li.active>a:hover, .nav-tabs>li.active>a:focus {
+      color:#fff;
+      background-color: #555555;
+     } 
+
   </style>
 </head>
 <body>
 
 <div class="container">
-  <h2 style="padding: 0 10px;">API Tester..</h2>
+  <h2>API Tester..</h2>
   <p style="padding: 20px;background-color: #dff0d8;">some text</p>
 
   <ul class="nav nav-tabs">
@@ -27,25 +66,25 @@
 
   <div class="tab-content">
     <div id="test1" class="tab-pane fade in active">
-      <h3>Test 1</h3>
+      <h4>Test 1</h4>
       <textarea id="ta1" style="width:100%; height:300px;padding:5px;">input...test1</textarea>
       <button onclick="test1();">Submit 1</button>
     </div>
 
     <div id="test2" class="tab-pane fade" style="padding: 20px;">
-      <h3>Test 2</h3>
+      <h4>Test 2</h4>
       <textarea id="ta2"style="width:100%; height:300px;padding:5px;;">input..test2</textarea>
       <button onclick="test2();">Submit 2</button>
     </div>
   
     <div id="test3" class="tab-pane fade" style="padding: 20px;">
-      <h3>Test 3</h3>
+      <h4>Test 3</h4>
       <textarea id="ta3"style="width:100%; height:300px;padding:5px;;">input..test3</textarea>
       <button onclick="test3();">Submit 3</button>
   </div>
   
     <div id="test4" class="tab-pane fade" style="padding: 20px;">
-      <h3>Test 4</h3>
+      <h4>Test 4</h4>
       <textarea id="ta4" style="width:100%; height:300px;padding:5px;;">input..test4</textarea>
       <button onclick="test4();">Submit 4</button>
     </div>
